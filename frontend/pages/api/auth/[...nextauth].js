25230dfe5b8355e6b38f26bd96b3f09e415b6353
andreocunha <andreoliveiracunha20@gmail.com>
2022-04-27 14:15:05 -0300

    email ufes

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -8,16 +8,16 @@ const options = {
             clientSecret: process.env.GOOGLE_SECRET,
         }),
     ],
-    // callbacks: {
-    //     async signIn(user, account, profile) {
-    //       if (account.provider === 'google' &&
-    //           profile.verified_email === true &&
-    //           profile.email.endsWith('@edu.ufes.br')) {
-    //         return true
-    //       } else {
-    //         return false
-    //       }
-    //     },
-    // }
+    callbacks: {
+        async signIn(user, account, profile) {
+          if (account.provider === 'google' &&
+              profile.verified_email === true &&
+              profile.email.endsWith('@edu.ufes.br')) {
+            return true
+          } else {
+            return false
+          }
+        },
+    }
 }
 export default (req, res) => NextAuth(req, res, options)
