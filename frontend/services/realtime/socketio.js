d7593dbedd8d8f78c4ad649f7025b2f191f34292
andreocunha <andreoliveiracunha20@gmail.com>
2022-05-03 14:13:57 -0300

    .

--- frontend/services/realtime/socketio.js
+++ frontend/services/realtime/socketio.js
@@ -2,6 +2,7 @@ import socketIOClient from 'socket.io-client';
 
 // const socket = socketIOClient(process.env.SOCKETIO_URL);
 // const socket = socketIOClient("localhost:4000");
+
 const socket = socketIOClient("ufes-forum-socket.herokuapp.com");
 
 export default socket;
